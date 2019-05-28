a91f9074f5030c28147c508a58c0520edcf19300
Steven Kirbach <steven.kirbach@gmail.com>
2019-05-28 14:59:54 -0700

    set DOTNET_ROOT from cmd rather than powershell
    we can't have them be strings, which seems to be unavoidable with powershell

--- eng/helix/configure-helix-machine.ps1
+++ eng/helix/configure-helix-machine.ps1
@@ -1,9 +1,4 @@
 # This file prepares the helix machine for our tests runs
-$dotnetLocation=Join-Path (Split-Path -Parent $script:MyInvocation.MyCommand.Path) "dotnet"
-
-# Set both DOTNET_ROOT environment variables. This is necessary, even for x64 runs.
-$env:DOTNET_ROOT=$dotnetLocation
-Set-Variable -Name "DOTNET_ROOT(x86)" -Value $dotnetLocation
 
 $runtimes = dotnet --list-runtimes
 foreach ($rt in $runtimes)
--- eng/helix/runtests.cmd
+++ eng/helix/runtests.cmd
@@ -1,1 +1,9 @@
+@echo off
+
+REM We set these variables here because they can't be strings
+REM and this is the easiest way of getting the correct path
+REM without turning it into a string through powershell
+set DOTNET_ROOT(x86)=%HELIX_CORRELATION_PAYLOAD%\dotnet
+set DOTNET_ROOT=%HELIX_CORRELATION_PAYLOAD%\dotnet
+
 powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0runtests.ps1"""  %*"
