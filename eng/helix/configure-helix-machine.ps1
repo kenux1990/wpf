cafea25f44a63eeb1e11d57db6a0992682436cfd
Steven Kirbach <steven.kirbach@gmail.com>
2019-05-27 18:11:14 -0700

    setting DOTNET_ROOT

--- eng/helix/configure-helix-machine.ps1
+++ eng/helix/configure-helix-machine.ps1
@@ -1,1 +1,3 @@
 # This file prepares the helix machine for our tests runs
+$dotnetLocation=Join-Path (Split-Path -Parent $script:MyInvocation.MyCommand.Path) "dotnet"
+$env:DOTNET_ROOT=$dotnetLocation
